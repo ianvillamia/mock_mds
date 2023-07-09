93c45d2b62fc2080324b72e6ac5b981bc32e8e46
Dean Villamia <kristian.dean_villamia@questronix.com.ph>
2023-07-09 21:04:14 +0800

    Pigeon working sample
    1. follow example in pigeon example
    2. change the appdelegate to include the changes (for some reason it doesn't modify it)
    3. add the generated (messages.g.swift) into Runner (right click add file to runner so that appdelegate will find the generated class)

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,51 @@
+// Copyright 2013 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// ignore_for_file: public_member_api_docs
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'src/messages.g.dart';
+
+// #docregion main-dart-flutter
+class _ExampleFlutterApi implements MessageFlutterApi {
+  @override
+  String flutterMethod(String? aString) {
+    return aString ?? '';
+  }
+}
+// #enddocregion main-dart-flutter
 
 void main() {
+
+    WidgetsFlutterBinding.ensureInitialized();
+// #docregion main-dart-flutter
+  MessageFlutterApi.setup(_ExampleFlutterApi());
+
+// #enddocregion main-dart-flutter
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Pigeon Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Pigeon Demo'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +54,71 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final ExampleHostApi _hostApi = ExampleHostApi();
+  String? _hostCallResult;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  // #docregion main-dart
+  final ExampleHostApi _api = ExampleHostApi();
+
+  /// Calls host method `add` with provided arguments.
+  Future<int> add(int a, int b) async {
+    try {
+      return await _api.add(a, b);
+    } catch (e) {
+      // handle error.
+      return 0;
+    }
+  }
+
+  /// Sends message through host api using `MessageData` class
+  /// and api `sendMessage` method.
+  Future<bool> sendMessage(String messageText) {
+    final MessageData message = MessageData(
+      code: Code.one,
+      data: <String?, String?>{'header': 'this is a header'},
+      description: 'uri text',
+    );
+    try {
+      return _api.sendMessage(message);
+    } catch (e) {
+      // handle error.
+      return Future<bool>(() => true);
+    }
+  }
+  // #enddocregion main-dart
+
+  @override
+  void initState() {
+    super.initState();
+    _hostApi.getHostLanguage().then((String response) {
+      setState(() {
+        _hostCallResult = 'Hello from $response!';
+      });
+    }).onError<PlatformException>((PlatformException error, StackTrace _) {
+      setState(() {
+        _hostCallResult = 'Failed to get host language: ${error.message}';
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _hostCallResult ?? 'Waiting for host language...',
             ),
+            if (_hostCallResult == null) const CircularProgressIndicator(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
